850510bf593dc057a6bfce3263b8c7fb216e14ea
Karoliina Kullamaa <karoliinaku@users.noreply.github.com>
2016-10-12 09:11:48 +0300

    kodutöö

---
+++ data.php
@@ -0,0 +1,107 @@
+﻿<?php
+
+	require("functions.php");
+	
+	//kas on sisse loginud, kui ei ole, siis suunata login lehele
+	
+	if (!isset ($_SESSION["userId"])) {
+		
+		header("Location: login.php");
+		exit();
+	}
+	
+	//kas ?logout on aadressireal
+	if (isset($_GET["logout"])) {
+		
+		session_destroy ();
+		header ("Location: login.php");
+		exit();
+	}
+
+	//ei ole tühjad väljad, mida salvestada (sugu ja värv)
+	if(isset($_POST["gender"]) &&
+			isset($_POST["color"]) &&
+			!empty($_POST["gender"]) &&
+			!empty($_POST["color"])
+		) {
+			savePeople($_POST["gender"], $_POST["color"]);
+			
+		}
+	
+	
+	$people = getAllPeople();
+	
+	echo "<pre>";
+	var_dump($people);
+	echo "</pre>";
+?>
+<h1>Data</h1>
+<p> 
+	Tere tulemast <?=$_SESSION["email"];?>!
+	<a href="?logout=1">Logi välja</a>
+</p>
+
+<form method="POST"> 
+<label>Sugu</label><br>
+			
+		<input type="text" name="gender" > <br><br>
+	
+<label>Värv</label><br>
+		
+		<input type="color" name="color" > <br><br>
+	
+	<input type="submit" value="Salvesta">	
+</form>
+
+<h2>Arhiiv</h2>
+<?php
+
+	foreach ($people as $p) {
+		
+		echo "<h3 style= ' color:".$p->color."; '>" .$p->gender."</h3>";
+	}
+?>
+
+<h2>Arhiivitabel</h2>
+<?php
+
+	$html = "<table>";
+	
+		$html .= "<tr>";
+			$html .= "<th>ID</th>";
+			$html .= "<th>Sugu</th>";
+			$html .= "<th>Värv</th>";
+			$html .= "<th>Loodud</th>";
+		$html .= "</tr>";
+
+	foreach($people as $p) {
+		$html .= "<tr>";
+			$html .= "<td>".$p->id."</td>";
+			$html .= "<td>".$p->gender."</td>";
+			$html .= "<td style=' background-color:".$p->color."; '>".$p->color."</td>";
+			$html .= "<td>".$p->created."</td>";
+		$html .= "</tr>";	
+	}
+
+	$html .= "</table>";
+	echo $html;
+	
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ functions.php
@@ -0,0 +1,161 @@
+﻿<?php
+
+	require("../../config.php");
+	//alustan sessiooni, et saaks kasutada $_SESSION muutujaid
+	session_start();
+
+	//****************
+	//*****SIGNUP*****
+	//****************
+	
+	$database = "if16_karoku";
+	
+	function signup ($email, $password, $username, $gender) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $email, $password);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+	}
+
+	
+	function login($email, $password) {
+		
+		$error = "";
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("SELECT id, email, password, created FROM user_sample WHERE email = ?");
+		
+		echo $mysqli->error;
+		
+		//asendan küsimärgi
+		$stmt->bind_param("s", $email);
+		
+		//määran tulpadele muutujad
+		$stmt->bind_result($id, $emailFromDatabase, $passwordFromDatabase, $created);
+		$stmt->execute();
+		
+		//küsin rea andmeid
+		if($stmt->fetch()) {
+			//oli rida
+			
+			//võrdlen paroole
+			$hash = hash ("sha512", $password);
+			if($hash == $passwordFromDatabase) {
+				echo "kasutaja ".$id." logis sisse";
+				
+				$_SESSION["userId"] = $id;
+				$_SESSION["email"] = $emailFromDatabase;
+				
+				//suunaks uuele lehele
+				header("Location: data.php");
+				exit();
+				
+			} else {
+				$error = "Parool on vale";
+			}
+			
+		} else {
+			//ei olnud
+			$error = "Sellise emailiga ".$email."kasutajat ei olnud";
+			
+		}
+		
+		return $error;
+		
+	}
+	
+	
+		function savePeople ($gender, $color) {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("INSERT INTO clothingOnTheCampus (gender, color) VALUES (?, ?)");
+		echo $mysqli->error;
+		
+		$stmt->bind_param("ss", $gender, $color);
+		
+		if ($stmt->execute()) {
+			echo "salvestamine õnnestus";
+		} else {
+			echo "ERROR".$stmt->error;
+		}
+	}
+	
+	function getAllPeople() {
+		
+		$mysqli = new mysqli($GLOBALS["serverHost"], $GLOBALS["serverUsername"], $GLOBALS["serverPassword"], $GLOBALS["database"]);
+	
+		$stmt = $mysqli->prepare("SELECT id, gender, color, created FROM clothingOnTheCampus");
+		
+		echo $mysqli->error;
+		
+		$stmt->bind_result($id, $gender, $color, $created);
+		$stmt->execute();
+		
+		//array("Karoliina", "Kullamaa")
+		$result = array();
+		
+		//seni kuni on üks rida andmeid saada (10 rida = 10 korda)
+		while ($stmt->fetch()) {
+			
+			$person = new StdClass();
+			$person->id = $id;
+			$person->gender = $gender;
+			$person->color = $color;
+			$person->created = $created;
+			
+			//echo $color."<br>";
+			array_push($result, $person);
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+		
+		return $result;
+	}
+	
+	function cleanInput($input) {
+		
+		$input = trim($input);
+		$input = stripslashes($input);
+		$input = htmlspecialchars($input);
+		
+		return $input;
+		
+	}
+	
+	
+	
+	
+	/*function sum ($x, $y) {
+		return $x + $y;
+		}
+
+	echo sum(375,555);
+	echo "<br>";
+	$answer = sum(10,15);
+	echo $answer;
+	echo "<br>";
+	
+	function hello ($firstname, $lastname) {
+		return "Tere tulemast ".$firstname." ".$lastname."!";
+	}
+	
+	echo sum(346,644);
+	echo "<br>";
+	$answer = sum(10,15);
+	echo $answer;
+	echo "<br>";
+	echo hello ("Karoliina", "K"); */
+?>
+
