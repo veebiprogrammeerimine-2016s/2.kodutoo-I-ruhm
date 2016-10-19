0a7806adc4d0941855c358e838db6496a66718c3
randotm <randotm@users.noreply.github.com>
2016-10-19 11:23:32 +0300

    No more duplicate emails or usernames.

--- functions.php
+++ functions.php
@@ -80,6 +80,23 @@
 	
 	function signup($email, $password, $username, $name, $d_o_b, $now) {
 		$mysqli = new mysqli( $GLOBALS['serverHost'], $GLOBALS['serverUsername'], $GLOBALS['serverPassword'], $GLOBALS['database']);
+		//Kontroll, kas kasutajanimi on olemas juba
+		$check_username_uniqueness_stmt = $mysqli->prepare("SELECT username FROM users WHERE username = ?");
+		$check_username_uniqueness_stmt->bind_param("s", $username);
+		$check_username_uniqueness_stmt->execute();
+		if($check_username_uniqueness_stmt->fetch()) {
+			echo "Kasutajanimi juba kasutusel";
+			return;
+		}
+		//Kontroll, kas email on olemas juba
+		$check_email_uniqueness_stmt = $mysqli->prepare("SELECT email FROM users WHERE email = ?");		
+		$check_email_uniqueness_stmt->bind_param("s", $email);
+		$check_email_uniqueness_stmt->execute();
+		if($check_email_uniqueness_stmt->fetch()) {
+			echo "Email juba kasutusel";
+			return;
+		}
+		//Kasutaja lisamine kasutajate tabelisse
 		$stmt = $mysqli->prepare ('INSERT INTO users (email, password, username, name, date_of_birth, created) VALUES (?,?,?,?,?,?)' );
 		echo $mysqli -> error;
 		$stmt -> bind_param ('ssssss', $email, $password, $username, $name, $d_o_b, $now);
@@ -88,7 +105,7 @@
 			echo 'Error'.$stmt->error;
 		}
 		
-		//tabeli tegemine kasutajale
+		//Tabeli tegemine kasutajale, kuhu salvestatakse tulemused
 		$mk_tbl_stmt = "CREATE TABLE ".$username." (
 		id INT(6) AUTO_INCREMENT PRIMARY KEY,
 		scramble VARCHAR(80) NOT NULL,
