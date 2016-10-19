1814fec0cf21d7c5790e897c6749d83b29c040ac
rasmaavi <rasmaavi@greeny.cs.tlu.ee>
2016-10-19 16:32:23 +0300

    added some lines

--- broneering.php
+++ broneering.php
@@ -1,8 +1,49 @@
 <?php
 require("functions.php");
 
+$place = "";
+$time = "";
+$people = "";
+$comment = "";
+$placeError ="";
+$timeError = "";
+$peopleError = "";
+$commentError = "";
 
-	if ( isset($_POST["place"]) &&
+	if (isset ($_POST["place"])) {
+		
+		if (empty ($_POST ["place"])) {
+			$placeError = "See väli on kohustuslik!";
+		} else {
+			$place = $_POST["place"];
+			   }	
+		}
+	if (isset ($_POST["time"])) {
+		
+		if (empty ($_POST ["time"])) {
+			$timeError = "See väli on kohustuslik!";
+		} else {
+			$time = $_POST["time"];
+		}
+	}
+	if (isset ($_POST["people"])) {
+		
+		if (empty ($_POST ["people"])) {
+			$peopleError = "See väli on kohustuslik!";
+		} else {
+			$people = $_POST["people"];
+			   }
+	}
+	if (isset ($_POST["comment"])) {
+		
+		if (empty ($_POST ["comment"])) {
+			$commentError = "See väli on kohustuslik!";
+		} else {
+			$comment = $_POST["comment"];
+		}
+	}
+		
+		if ( isset($_POST["place"]) &&
 		isset($_POST["time"]) &&
 		isset($_POST["people"]) &&
 		isset($_POST["comment"]) &&
@@ -19,24 +60,24 @@ require("functions.php");
 			  
 	}
 	
-	elseif (
+	// elseif (
 		
-		isset($_POST["place"]) &&
-		isset($_POST["time"]) &&
-		isset($_POST["people"]) &&
-		isset($_POST["comment"]) &&
-		(empty($_POST["place"]) OR
-		empty($_POST["time"]) OR
-		empty($_POST["people"]) OR
-		empty($_POST["comment"]) 
+		// isset($_POST["place"]) &&
+		// isset($_POST["time"]) &&
+		// isset($_POST["people"]) &&
+		// isset($_POST["comment"]) &&
+		// (empty($_POST["place"]) OR
+		// empty($_POST["time"]) OR
+		// empty($_POST["people"]) OR
+		// empty($_POST["comment"]) 
 	
-		)
+		// )
 		
-	) {
+	// ) {
 	
-		echo "täida kõik väljad!";
+		// echo "täida kõik väljad!";
 		
-	}
+	// }   EI OSANUD PANNA NII, ET JÄTAKS VÄLJAD TÄIDETUKS KUI MÕNI TÜHJAKS JÄI
 	
 	$reservation = getAllReservation();
 
@@ -48,8 +89,8 @@ require("functions.php");
 <form method="POST">
 <label>Millisesse kohta soovite lauda broneerida?</label>
 <br><br>
-<select name="place" type="place">
-  <option value="other">Vali koht...</option>
+<select name="place" type="place" value="<?php echo $place; ?>"> <?php echo $placeError;  ?>
+  <option value="Vali koht">Vali koht...</option>
   <option value="butterfly">Butterfly Lounge</option>
   <option value="tuljak">Tuljak</option>
   <option value="wesset">Villa Wesset</option>
@@ -62,19 +103,19 @@ require("functions.php");
 
 <label>Mis kell saabute?</label>
 <br><br>
-<input name="time" type="time" placeholder="Kellaaeg">
+<input name="time" type="time" placeholder="Kellaaeg" value="<?php echo $time; ?>"> <?php echo $timeError;  ?>
 <br><br>
 
 	
 
 <label>Mitu inimest teid on?</label>
 <br><br>
-<input name="people" type="people" placeholder="Mitu inimest?">
+<input name="people" type="people" placeholder="Mitu inimest?" value="<?php echo $people; ?>"> <?php echo $peopleError;  ?>
 <br><br>
 
 <label>Siia kirjutage oma erisoovid.</label>
 <br><br>
-<input name="comment" type="comment" placeholder="Erisoov" style="width: 500px;"> 
+<input name="comment" type="comment" placeholder="Erisoov" style="width: 500px;" value="<?php echo $comment; ?>"> <?php echo $commentError;  ?>
 <br><br>
 
 <br><br>
